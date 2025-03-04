c511da0d1e1f5146cf4d0c79bfcff2b7f1c0091f
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-03-04 12:12:46 -0300

    feat: add container consul service, and module consul

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
+import { ConsulModule } from './consul/consul.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(
       {
         validate
-      })
+      }),
+    ConsulModule
   ],
   controllers: [],
   providers: [],
